9de1935a5d072584f9e0ffc5f89af95fcebf4ef8
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-07-22 18:31:11 -0700

    Add ImageProfile model

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+import six
+
+@six.python_2_unicode_compatible
+class ImagerProfile(models.Model):
+    user = models.OneToOneField(User)
+    fav_camera = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    web_url = models.URLField()
+    type_photography = models.CharField(max_length=30)
+
