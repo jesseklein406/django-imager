e6db95cce0239d9e8ce33aec5cf21aa1bd19df03
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-22 18:31:17 -0700

    Add __str__ method

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,7 +1,8 @@
+import six
 
 from django.db import models
 from django.contrib.auth.models import User
-import six
+
 
 @six.python_2_unicode_compatible
 class ImagerProfile(models.Model):
@@ -10,4 +11,7 @@ class ImagerProfile(models.Model):
     address = models.CharField(max_length=100)
     web_url = models.URLField()
     type_photography = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "{}'s profile".format(self.user.username)
 
