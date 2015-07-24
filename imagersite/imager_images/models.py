4f03a1862aa9b49510f1dee67e60356ff992c696
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-24 16:54:36 -0700

    Frame out photo model

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,8 +1,31 @@
+import six
+
 from django.db import models
+from django.contrib.auth.models import User
 
 
+PUBLISHED_CHOICES = (
+    ('private', 'private'),
+    ('shared', 'shared'),
+    ('public', 'public')
+)
+
+
+@six.python_2_unicode_compatible
 class Photo(models.Model):
     file = models.ImageField(upload_to='photo_files/%Y-%m-%d')
-    
-
+    user = models.ForeignKey(
+        User,
+        null=False
+    )
+    title = models.CharField()
+    description = models.TextField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField()
+    published = models.CharField(
+        maxlength=256,
+        choices=PUBLISHED_CHOICES,
+        default='private'
+    )
   
