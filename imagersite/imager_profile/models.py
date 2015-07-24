16742262b6a37f34bf83b3b6d6bcfd72e69276b2
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-23 17:49:17 -0700

    Make sure a user has profile

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,16 +1,27 @@
 import six
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import ActiveProfileManager, User
 
 
 @six.python_2_unicode_compatible
 class ImagerProfile(models.Model):
-    user = models.OneToOneField(User)
-    fav_camera = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
+    user = models.OneToOneField(
+        User,
+        nullable=False
+    )
+    fav_camera = models.CharField(
+        max_length=30
+    )
+    address = models.CharField()
     web_url = models.URLField()
     type_photography = models.CharField(max_length=30)
 
+    objects = models.Manager()
+    active = ActiveProfileManager()
+
     def __str__(self):
         return "{}'s profile".format(self.user.username)
+
+    def is_active(self):
+        return self.user.is_active
