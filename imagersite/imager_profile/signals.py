d126647876de8260a1335ebe5bb09e800a6e520b
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-23 18:07:20 -0700

    Add first pass signal functions

--- imagersite/imager_profile/signals.py
+++ imagersite/imager_profile/signals.py
@@ -18,6 +18,31 @@ def create_profile_for_user(sender, **kwargs):
         instance.profile.save()
 
 
+@receiver(post_save, sender=ImagerProfile)
+def create_user_for_profile(sender, **kwargs):
+    """Add a related user to profile if needed."""
+    instance = kwargs.get('instance')
+    if not instance or kwargs.get('raw', False):
+        return
+    try:
+        instance.user
+    except User.UserDoesNotExist:
+        instance.user = User()
+        instance.user.save()
+
+
+@receiver(post_delete, sender=User)
+def delete_profile_for_user(sender, kwargs):
+    """Delete a related user when profile is deleted."""
+    instance = kwargs.get('instance')
+    if not instance:
+        return
+    try:
+        instance.profile.delete()
+    except ImagerProfile.DoesNotExist:
+        pass
+
+
 @receiver(post_delete, sender=ImagerProfile)
 def delete_user_for_profile(sender, kwargs):
     """Delete a related user when profile is deleted."""
