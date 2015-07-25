200652db0fe36d53200e5c68089a254b5afc388c
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-24 17:34:41 -0700

    Add album model

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -13,12 +13,12 @@ PUBLISHED_CHOICES = (
 
 @six.python_2_unicode_compatible
 class Photo(models.Model):
-    file = models.ImageField(upload_to='photo_files/%Y-%m-%d')
     user = models.ForeignKey(
         User,
         null=False
     )
-    title = models.CharField()
+    photo = models.ImageField(upload_to='photo_files/%Y-%m-%d')
+    title = models.CharField(maxlength=256)
     description = models.TextField()
     date_uploaded = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
@@ -28,4 +28,35 @@ class Photo(models.Model):
         choices=PUBLISHED_CHOICES,
         default='private'
     )
-  
+
+    def __str__(self):
+        return "Photo: {}".format(self.title)
+
+
+@six.python_2_unicode_compatible
+class Album(models.Model):
+    user = models.ForeignKey(
+        User,
+        null=False
+    )
+    photos = models.ManyToManyField(
+        Photo,
+        related_name='album'
+    )
+    title = models.CharField(maxlength=256)
+    description = models.TextField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField()
+    published = models.CharField(
+        maxlength=256,
+        choices=PUBLISHED_CHOICES,
+        default='private'
+    )
+    cover = models.ForeignKey(
+        Photo,
+        related_name='cover_photo'
+    )
+
+    def __str__(self):
+        return "Album: {}".format(self.title)
