5e10de59cd7ae741329cc79e3aff89580e2fb862
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-26 13:30:58 -0700

    Refactor album test case

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -49,19 +49,19 @@ class PhotoTestCase(TestCase):
         user1 = UserFactory()
         user1.set_password('secret')
         user1.save()
-        for i in range(100):
+        for i in range(10):
             PhotoFactory(user=user1)
 
     def tearDown(cls):
         User.objects.all().delete()
 
     def test_photo_creation(self):
-        self.assertTrue(Photo.objects.count() == 100)
+        self.assertEqual(Photo.objects.count(), 10)
 
     def test_photos_belong_to_user(self):
         user1 = User.objects.first()
         pic1 = user1.photos.first()
-        self.assertEqual(user1.photos.count(), 100)
+        self.assertEqual(user1.photos.count(), 10)
         self.assertEqual(user1.username, pic1.user.username)
 
     def test_photo_do_not_belong_to_other_users(self):
@@ -70,10 +70,56 @@ class PhotoTestCase(TestCase):
         other_user.save()
         self.assertEqual(other_user.photos.count(), 0)
 
+    def test_photo_deletion_on_user_deletion(self):
+        user1 = User.objects.first()
+        user1.delete()
+        self.assertEqual(Photo.objects.count(), 0)
+
 
 class AlbumTestCase(TestCase):
     """Test albums can be created and assigned to users and albums."""
     @classmethod
     def setUp(cls):
-        pass
-        
+        cls.user1 = UserFactory()
+        cls.user1.set_password('secret')
+        cls.user1.save()
+        cls.album1 = AlbumFactory(user=cls.user1)
+        cls.album2 = AlbumFactory(user=cls.user1)
+        cls.pics = []
+        for i in range(5):
+            pic = PhotoFactory(user=cls.user1)
+            pic.save()
+            cls.pics.append(pic)
+
+    def tearDown(cls):
+        User.objects.all().delete()
+
+    def test_album_creation(self):
+        self.assertEqual(Album.objects.count(), 2)
+
+    def test_albums_belong_to_user(self):
+        self.assertEqual(self.user1.albums.count(), 2)
+        self.assertEqual(self.user1.username, self.album1.user.username)
+
+    def test_albums_do_not_belong_to_other_users(self):
+        other_user = UserFactory()
+        other_user.set_password('moresecret')
+        other_user.save()
+        self.assertEqual(other_user.albums.count(), 0)
+
+    def test_add_photos_to_albums(self):
+        self.assertEqual(self.album1.photos.count(), 0)
+        self.album1.photos.add(*self.pics[:3])
+        self.album2.photos.add(*self.pics[3:])
+        self.assertEqual(self.album1.photos.count(), 3)
+        self.assertEqual(self.album2.photos.count(), 2)
+
+    def test_set_album_cover_photo(self):
+        self.assertIsNone(self.album1.cover)
+        self.album1.cover = self.pics[0]
+        self.assertIsInstance(self.album1.cover, Photo)
+
+    def test_album_deletion_on_user_deletion(self):
+        self.user1.delete()
+        self.assertEqual(Album.objects.count(), 0)
+
