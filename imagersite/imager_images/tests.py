199caafc817e4e007b2eedd307cb7bff06c029c6
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-25 22:48:11 -0700

    Add a UserFactory for images test

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -5,6 +5,22 @@ import factory
 from faker import Faker
 
 from imager_profile.models import ImagerProfile
-from .models import Album, Photo
+from .models import Album, Pho
 
 # Create your tests here.
+fake = Faker()
+
+
+class UserFactory(factory.Factory):
+    """Create a fake user."""
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = fake.email()
+
+
+
+    
