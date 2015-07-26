d4dc22be443fb73157d542f86dbee5d89ac5a713
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-25 22:29:56 -0700

    Add imports to imager_images test

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
 from django.test import TestCase
+import factory
+from faker import Faker
+
+from imager_profile.models import ImagerProfile
+from .models import Album, Photo
 
 # Create your tests here.
