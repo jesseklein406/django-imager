c236f641757de1bc9cab4f1e099ed7abc6b70e3d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-27 23:58:00 -0700

    Move site level static files

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -115,6 +115,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "imagersite/static"),
+)
 
 # Media file handling
 MEDIA_URL = '/media/'
