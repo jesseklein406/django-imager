38d12647905cd399b7218721af16d12197c27840
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-03 01:54:13 -0700

    Change database to postgresql

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -91,11 +91,12 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3')
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-imager',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
