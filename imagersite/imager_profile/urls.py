17bacdaba60aea7b272679f4ff4fc65023edd94d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-02 18:38:53 -0700

    Add generic UpdateView for profile

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import ProfileDetailView
+from .views import ProfileDetailView, ProfileUpdateView
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', ProfileDetailView.as_view(), name='detail'),
+    url(r'^edit/', ProfileUpdateView.as_view(), name='edit')
 )
 
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 from braces.views import LoginRequiredMixin
 
 from .models import ImagerProfile
@@ -9,6 +9,14 @@ from .models import ImagerProfile
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = ImagerProfile
     template_name = "imager_profile/profile_detail.html"
+
+    def get_object(self):
+        return self.request.user
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = ImagerProfile
+    fields = ('camera', 'address', 'web_url', 'type_photography')
 
     def get_object(self):
         return self.request.user
