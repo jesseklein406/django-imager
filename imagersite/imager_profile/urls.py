10023bbdd1c7058d614b747f044baa51fb6681fb
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-02 21:59:25 -0700

    remove unused line from aborted CBV in urls

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -6,7 +6,6 @@ from .views import ProfileDetailView, profile_update_view
 urlpatterns = patterns(
     '',
     url(r'^$', ProfileDetailView.as_view(), name='detail'),
-    # url(r'^edit/(?P<pk>\d+)', ProfileUpdateView.as_view(), name='edit')
     url(r'^edit/$', profile_update_view, name='edit')
 )
 
