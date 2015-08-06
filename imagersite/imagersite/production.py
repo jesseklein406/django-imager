3d90fd5977ce1ab7e1dae1e8e1542e1e2768699d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-06 16:48:11 -0700

    Remove production.py

--- imagersite/imagersite/production.py
+++
@@ -1,8 +0,0 @@
-from settings import *
-
-DEBUG = False
-TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = [
-    'ec2-52-24-98-74.us-west-2.compute.amazonaws.com', 'localhost'
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
