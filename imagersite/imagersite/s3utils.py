b650d08031fdbcde311c9f054ca317a8501af1cc
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-07 16:26:20 -0700

    Fix typo

--- imagersite/imagersite/s3utils.py
+++ imagersite/imagersite/s3utils.py
@@ -15,6 +15,6 @@ class StaticS3BotoStorage(S3BotoStorage):
 class MediaS3BotoStorage(S3BotoStorage):
     """Storage for uploaded media files."""
     def __init__(self, *args, **kwargs):
-        kwargs['location'] = settings.STATIC_DIRECTORY
-        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
+        kwargs['location'] = settings.MEDIA_DIRECTORY
+        super(MediaS3BotoStorage, self).__init__(*args, **kwargs)
 
