8479c8aab6fcdbc249849a405be6770d0677ad7d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-07 16:23:38 -0700

    Change S3BotoStorage to class based

--- imagersite/imagersite/s3utils.py
+++ imagersite/imagersite/s3utils.py
@@ -1,6 +1,20 @@
 from storages.backends.s3boto import S3BotoStorage
+from django.conf import settings
 
 
-StaticS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaS3BotoStorage = lambda: S3BotoStorage(location='media')
+# StaticS3BotoStorage = lambda: S3BotoStorage(location='static')
+# MediaS3BotoStorage = lambda: S3BotoStorage(location='media')
+
+class StaticS3BotoStorage(S3BotoStorage):
+    """Storage for static files."""
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.STATIC_DIRECTORY
+        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
+
+
+class MediaS3BotoStorage(S3BotoStorage):
+    """Storage for uploaded media files."""
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.STATIC_DIRECTORY
+        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
 
