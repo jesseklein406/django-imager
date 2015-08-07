766f98373fb2360dde6c4a43cb36ad91b57165aa
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-07 16:49:33 -0700

    Set thumbnail debug to true

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
-THUMBNAIL_DEBUG = DEBUG
+THUMBNAIL_DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
 
