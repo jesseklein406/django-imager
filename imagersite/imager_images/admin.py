72c0b6580cbc7751bedde1448b1d9865a8ab814d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-19 06:19:45 -0700

    Have face boxes drawn; need to fix alignment

--- imagersite/imager_images/admin.py
+++ imagersite/imager_images/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Photo, Album
+from .models import Photo, Album, Face
 
 
 admin.site.register(Photo)
 admin.site.register(Album)
+admin.site.register(Face)
